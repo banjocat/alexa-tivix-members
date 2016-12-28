4ec60bbd5852bf5ca81eb9dbf0b46eac7264ebfb
banjocat <jackmuratore@gmail.com>
2016-12-27 23:31:57 -0500

    Add working random bio function

--- scraper/tivix.py
+++ scraper/tivix.py
@@ -4,12 +4,25 @@ import requests
 from lxml import html
 
 def get_random_tivix_member_bio():
+    '''
+    returns a string of a randomly chosen tivix member bio
+    '''
     members = get_list_of_tivix_members()
     member_url = random.choice(members)
-    page = requests.get('http://www.tivix.com/team-members' + member_url)
-    return 'Nothing yet'
+    page = requests.get('http://www.tivix.com' + member_url)
+    tree = html.fromstring(page.content)
+    name = tree.xpath('//h1[@class="member-name"]/text()')[0]
+    position = tree.xpath('//h2[@class="member-title"]/text()')[0]
+    bio_list = tree.xpath('//div[@class="member-bio"]/div//text()')
+    bio = ' '.join(bio_list)
+    # Creating the full text with correct puncutation
+    full_text = '%s. %s. %s.' % (name, position, bio)
+    return full_text
 
 def get_list_of_tivix_members():
+    '''
+    returns an array of tivix member bio url paths
+    '''
     page = requests.get('http://www.tivix.com/team-members/')
     tree = html.fromstring(page.content)
     members = tree.xpath('//div[@class="team-member"]/a/attribute::href')
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from scraper.tivix import get_list_of_tivix_members
+from scraper.tivix import get_random_tivix_member_bio
 
 def test_get_all_tivix_members():
     members = get_list_of_tivix_members()
@@ -8,4 +9,8 @@ def test_get_all_tivix_members():
     assert '/team-members/tan-nguyen/' in members
     assert '/team-members/will-liu/' in members
     assert '/team-members/george-bush/' not in members
+
+def test_output_random_bio():
+    bio = get_random_tivix_member_bio()
+    print(bio)
 

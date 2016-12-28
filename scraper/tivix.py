dd13c52639a7f8159fde33199cc5fc2bd4dbf87b
banjocat <jackmuratore@gmail.com>
2016-12-27 23:10:26 -0500

    Unit test pass

--- scraper/tivix.py
+++ scraper/tivix.py
@@ -1,9 +1,16 @@
+import random
+
 import requests
 from lxml import html
 
 def get_random_tivix_member_bio():
+    members = get_list_of_tivix_members()
+    member_url = random.choice(members)
+    page = requests.get('http://www.tivix.com/team-members' + member_url)
     return 'Nothing yet'
 
 def get_list_of_tivix_members():
     page = requests.get('http://www.tivix.com/team-members/')
     tree = html.fromstring(page.content)
+    members = tree.xpath('//div[@class="team-member"]/a/attribute::href')
+    return members
--- tests.py
+++ tests.py
@@ -3,9 +3,9 @@ from scraper.tivix import get_list_of_tivix_members
 def test_get_all_tivix_members():
     members = get_list_of_tivix_members()
     assert members
-    assert 'jack-muratore' in  members
-    assert 'kyle-conners' in members
-    assert 'tan-nguyen' in members
-    assert 'will-liu' in members
-    assert 'george-bush' not in members
+    assert '/team-members/jack-muratore/' in  members
+    assert '/team-members/kyle-connors/' in members
+    assert '/team-members/tan-nguyen/' in members
+    assert '/team-members/will-liu/' in members
+    assert '/team-members/george-bush/' not in members
 
